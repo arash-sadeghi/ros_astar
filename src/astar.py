eb0ed178b21dd5df481b4d32c3088cc9954ee0e6
arash <arash.sadeghi1997@gmail.com>
2023-08-13 02:14:20 +0000

    astar working

--- src/astar.py
+++ src/astar.py
@@ -2,94 +2,108 @@
 
 import rospy
 from nav_msgs.msg import OccupancyGrid
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Int32MultiArray
 import heapq
-import pdb 
-class AStarNode:
 
+class AStarPathPlanner:
     def __init__(self):
-        rospy.init_node('a_star_node')
+        rospy.init_node('astar')
 
         self.map = None
         self.start = None
         self.goal = None
 
         self.map_sub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
-        self.start_sub = rospy.Subscriber('/start_position', Point, self.start_callback)
-        self.goal_sub = rospy.Subscriber('/goal_position', Point, self.goal_callback)
-
-        self.path_pub = rospy.Publisher('/path', Int32MultiArray, queue_size=1)
-
-        rospy.spin()
+        self.start_sub = rospy.Subscriber('/start_position', PoseStamped, self.start_callback)
+        self.goal_sub = rospy.Subscriber('/goal_position', PoseStamped, self.goal_callback)
+        self.path_pub = rospy.Publisher('/path', Int32MultiArray, queue_size=10)
 
     def map_callback(self, msg):
-        self.map = msg.data
-        self.width = msg.info.width
-        self.height = msg.info.height
-        self.resolution = msg.info.resolution
+        self.map = msg
 
     def start_callback(self, msg):
-        self.start = (int(msg.x / self.resolution), int(msg.y / self.resolution))
+        if self.map is None:
+            return 
+        self.start = (int(msg.pose.position.x/self.map.info.resolution), int(msg.pose.position.y/self.map.info.resolution))
 
     def goal_callback(self, msg):
-        self.goal = (int(msg.x / self.resolution), int(msg.y / self.resolution))
+        if self.map is None:
+            return 
+        self.goal = (int(msg.pose.position.x/self.map.info.resolution), int(msg.pose.position.y/self.map.info.resolution))
 
-        if self.map is not None and self.start is not None:
-            path = self.a_star()
-            self.publish_path(path)
+    def heuristic(self, node):
+        return abs(node[0] - self.goal[0]) + abs(node[1] - self.goal[1])
 
-    def heuristic(self, a, b):
-        return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-    def a_star(self):
-        open_list = [(0, self.start)]
+    def astar(self):
+        if self.map is None or self.start is None or self.goal is None:
+            return []
+        open_heap = [(0, self.start)]
         came_from = {}
+        g_scores = {node: float('inf') for node in self.get_nodes()}
+        g_scores[self.start] = 0
+        f_scores = {node: float('inf') for node in self.get_nodes()}
+        f_scores[self.start] = self.heuristic(self.start)
 
-        while open_list:
-            current_cost, current = heapq.heappop(open_list)
+        while open_heap:
+            current_f, current = heapq.heappop(open_heap)
 
             if current == self.goal:
                 path = []
+                print(f"came_from {came_from}")
                 while current in came_from:
                     path.append(current)
                     current = came_from[current]
-                print(f"path found rom {self.start} to {self.goal}  path: {path}")
-                return path[::-1]
+                path.append(self.start)
+                path.reverse()
+                return path
 
             for neighbor in self.get_neighbors(current):
-                cost = current_cost + 1
-                try:
-                    statement = neighbor not in came_from or cost < came_from[neighbor]
-                except Exception as E:
-                    print("[-]",E) 
-                    pdb.set_trace()
-                if statement:
+                tentative_g = g_scores[current] + self.heuristic(neighbor)
+                if tentative_g < g_scores[neighbor]:
                     came_from[neighbor] = current
-                    heapq.heappush(open_list, (cost + self.heuristic(neighbor, self.goal), neighbor))
+                    g_scores[neighbor] = tentative_g
+                    f_scores[neighbor] = g_scores[neighbor] + self.heuristic(neighbor)
+                    if neighbor not in [node for _, node in open_heap]:
+                        heapq.heappush(open_heap, (f_scores[neighbor], neighbor))
 
-        print(f"path not found from {self.start} to {self.goal} with resolution {self.resolution}")
+        print(f"path not found from {self.start} to {self.goal}")
         return []
 
-    def get_neighbors(self, pos):
+    def get_nodes(self):
+        nodes = []
+        for x in range(self.map.info.width):
+            for y in range(self.map.info.height):
+                if self.map.data[x + y * self.map.info.width] == 0:
+                    nodes.append((x, y))
+        return nodes
+
+    def get_neighbors(self, node):
         neighbors = []
+        x, y = node
         for dx in [-1, 0, 1]:
             for dy in [-1, 0, 1]:
-                if dx == 0 and dy == 0:
-                    continue
-                new_x = pos[0] + dx
-                new_y = pos[1] + dy
-                # pdb.set_trace()
-                if 0 <= new_x < self.width and 0 <= new_y < self.height and self.map[new_y * self.width + new_x] == 0:
+                new_x = x + dx
+                new_y = y + dy
+                if (new_x, new_y) != node and (0 <= new_x < self.map.info.width) and (0 <= new_y < self.map.info.height) and self.map.data[new_x + new_y * self.map.info.width] == 0:
                     neighbors.append((new_x, new_y))
         return neighbors
 
     def publish_path(self, path):
-        path_msg = Int32MultiArray(data=path)
+        path_msg = Int32MultiArray(data=[node[0] + node[1] * self.map.info.width for node in path])
         self.path_pub.publish(path_msg)
+
+    def run(self):
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            path = self.astar()
+            self.publish_path(path)
+            rate.sleep()
 
 if __name__ == '__main__':
     try:
-        AStarNode()
+        print("[+] astar started")
+        planner = AStarPathPlanner()
+        planner.run()
     except rospy.ROSInterruptException:
         pass
--- src/pos_publisher.py
+++ src/pos_publisher.py
@@ -2,21 +2,23 @@
 
 import rospy
 from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseStamped
 
 rospy.init_node('pos_publisher')
 
 
-s_pub = rospy.Publisher('/start_position', Point, queue_size=1)
-g_pub = rospy.Publisher('/goal_position', Point, queue_size=1)
+s_pub = rospy.Publisher('/start_position', PoseStamped, queue_size=1)
+g_pub = rospy.Publisher('/goal_position', PoseStamped, queue_size=1)
 
-s = Point()
-s.x = s.y = 15
+s = PoseStamped()
+s.pose.position.x = s.pose.position.y = 15
 
-g = Point()
-g.x = 13
-g.y = 15
+g = PoseStamped()
+g.pose.position.x = 13
+g.pose.position.y = 15
 
 while not rospy.is_shutdown():
     s_pub.publish(s)
     g_pub.publish(g)
+    print(f"published {s} to {g}")
 
